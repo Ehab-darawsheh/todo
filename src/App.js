46a68020f4dc628b7870b24f1d16155f7226df79
Ehab-darawsheh <ehabdarawsheh07@gmail.com>
2022-11-01 02:20:40 +0300

    add delete button to the todos

--- src/App.js
+++ src/App.js
@@ -2,26 +2,30 @@ import React, { useEffect, useState } from "react";
 import { Button, FormControl, Input, InputLabel } from "@mui/material";
 import "./App.css";
 import Todo from "./Todo";
-import db from './firebase';
-import firebase from 'firebase/compat/app';
+import db from "./firebase";
+import firebase from "firebase/compat/app";
 
 function App(props) {
   const [todos, setTodos] = useState([]);
   const [input, setInput] = useState("");
 
   useEffect(() => {
-    db.collection('todos').orderBy('timestamp','desc').onSnapshot((snapshot) => {
-      setTodos(snapshot.docs.map(doc => doc.data().todo))
-    })
-  },[])
+    db.collection("todos")
+      .orderBy("timestamp", "desc")
+      .onSnapshot((snapshot) => {
+        setTodos(
+          snapshot.docs.map((doc) => ({ id: doc.id, todo: doc.data().todo }))
+        );
+      });
+  }, []);
 
   const addTodo = (e) => {
     e.preventDefault();
 
-    db.collection('todos').add({
-      todo:input,
-      timestamp: firebase.firestore.FieldValue.serverTimestamp()
-    })
+    db.collection("todos").add({
+      todo: input,
+      timestamp: firebase.firestore.FieldValue.serverTimestamp(),
+    });
     setTodos([...todos, input]);
     setInput("");
   };
--- src/Todo.js
+++ src/Todo.js
@@ -1,15 +1,18 @@
-import { List, ListItem, ListItemText } from "@mui/material";
+import {  List, ListItem, ListItemText } from "@mui/material";
 import React from "react";
-
+import db from "./firebase";
 import './Todo.css'
+import DeleteIcon  from '@mui/icons-material/Delete';
 
 const Todo = (props) => {
+
   return (
     <div className="todo">
     <List>
       <ListItem>
-        <ListItemText primary={props.todo} />
-      </ListItem>
+        <ListItemText primary={props.todo.todo} />
+         <DeleteIcon onClick={e => db.collection('todos').doc(props.todo.id).delete()} />
+       </ListItem>
     </List>
     </div>
   );
